833671e3637829ba2e0598fecc0e5742aebd9b29
田辺 <katuobrothers@icloud.com>
2023-06-22 00:01:26 +0900

    fix: アイテムプレートの向きを調整中

--- Assets/MyFPS/Scripts/View/ItemInfoPlate.cs
+++ Assets/MyFPS/Scripts/View/ItemInfoPlate.cs
@@ -19,8 +19,21 @@ public class ItemInfoPlate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        CustumLookAt();
     }
+    [SerializeField] Vector3 f;
+    private void CustumLookAt()
+    {
+        // ターゲットへの向きベクトル計算
+        var dir = Camera.main.transform.position - transform.position;
 
+        // ターゲットの方向への回転
+        var lookAtRotation = Quaternion.LookRotation(dir, Vector3.up);
+        // 回転補正
+        var offsetRotation = Quaternion.FromToRotation(f, Vector3.forward);
+
+        // 回転補正→ターゲット方向への回転の順に、自身の向きを操作する
+        transform.rotation = lookAtRotation * offsetRotation;
+    }
     
 }
