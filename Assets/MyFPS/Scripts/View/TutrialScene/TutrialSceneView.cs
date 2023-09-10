a087c293597a589765b1eb54d9965d2a219fc28a
田辺 <katuobrothers@icloud.com>
2023-09-10 11:30:35 +0900

    feat: ニックネーム入力画面UI作成中

--- Assets/MyFPS/Scripts/View/TutrialScene/TutrialSceneView.cs
+++ Assets/MyFPS/Scripts/View/TutrialScene/TutrialSceneView.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class TutrialSceneView : MonoBehaviour
 {
+    public TMP_InputField inputField;
+    public Button submitButton;
+
     // Start is called before the first frame update
     void Start()
     {
